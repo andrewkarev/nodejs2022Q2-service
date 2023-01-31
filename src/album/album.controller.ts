b75c3ba0694519cf6cc267257cf51587f601dc2b
Andrei Karev <an.carev4@gmail.com>
2023-01-31 15:22:09 +0300

    feat: implement album controller

--- src/album/album.controller.ts
+++ src/album/album.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Put,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AlbumService } from './album.service';
+import { AlbumDTO } from './dto';
 
 @Controller('album')
-export class AlbumController {}
+export class AlbumController {
+  constructor(private albumService: AlbumService) {}
+
+  @Get()
+  getAlbums() {
+    return this.albumService.getAlbums();
+  }
+
+  @Get(':id')
+  getAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumService.getAlbum(id);
+  }
+
+  @Post()
+  createAlbum(@Body(ValidationPipe) dto: AlbumDTO) {
+    return this.albumService.createAlbum(dto);
+  }
+
+  @Put(':id')
+  updateAlbumInfo(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body(ValidationPipe) dto: AlbumDTO,
+  ) {
+    return this.albumService.updateAlbumInfo(id, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  deleteAlbum(@Param('id', ParseUUIDPipe) id: string) {
+    return this.albumService.deleteAlbum(id);
+  }
+}
