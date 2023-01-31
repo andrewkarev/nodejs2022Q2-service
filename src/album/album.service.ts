a565b2e4c86751ddc08a6f06238ce02df911c347
Andrei Karev <an.carev4@gmail.com>
2023-01-31 15:22:03 +0300

    feat: implement album service

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid4 } from 'uuid';
+import { DbMessages } from 'src/common/DbMessages';
+import { DBService } from 'src/db/db.service';
+import { AlbumDTO } from './dto';
 
 @Injectable()
-export class AlbumService {}
+export class AlbumService {
+  constructor(private db: DBService) {}
+
+  async getAlbums() {
+    return await this.db.getAlbums();
+  }
+
+  async getAlbum(albumId: string) {
+    const response = await this.db.getAlbum(albumId);
+
+    if (!response) throw new NotFoundException();
+
+    return response;
+  }
+
+  async createAlbum(dto: AlbumDTO) {
+    const album = { ...dto, id: uuid4() };
+
+    return await this.db.createAlbum(album);
+  }
+
+  async updateAlbumInfo(albumId, dto) {
+    const response = await this.db.updateAlbumInfo(albumId, dto);
+
+    if (response === DbMessages.NOT_FOUND) throw new NotFoundException();
+
+    return response;
+  }
+
+  async deleteAlbum(albumId) {
+    const response = await this.db.deleteAlbum(albumId);
+
+    if (response === DbMessages.NOT_FOUND) throw new NotFoundException();
+
+    return response;
+  }
+}
