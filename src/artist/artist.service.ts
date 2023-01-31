89a3b21275f1b5d3bde1b2669da7dffc72cd8321
Andrei Karev <an.carev4@gmail.com>
2023-01-31 14:59:09 +0300

    feat: implement artist service

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,4 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DbMessages } from 'src/common/DbMessages';
+import { DBService } from 'src/db/db.service';
+import { v4 as uuid4 } from 'uuid';
+import { ArtistDTO } from './dto';
 
 @Injectable()
-export class ArtistService {}
+export class ArtistService {
+  constructor(private db: DBService) {}
+
+  async getArtists() {
+    return await this.db.getArtists();
+  }
+
+  async getArtist(artistId: string) {
+    const response = await this.db.getArtist(artistId);
+
+    if (!response) throw new NotFoundException();
+
+    return response;
+  }
+
+  async createArtist(dto: ArtistDTO) {
+    const artist = { ...dto, id: uuid4() };
+
+    return await this.db.createArtist(artist);
+  }
+
+  async updateArtistInfo(artistId, dto) {
+    const response = await this.db.updateArtistInfo(artistId, dto);
+
+    if (response === DbMessages.NOT_FOUND) throw new NotFoundException();
+
+    return response;
+  }
+
+  async deleteArtist(artistId) {
+    const response = await this.db.deleteArtist(artistId);
+
+    if (response === DbMessages.NOT_FOUND) throw new NotFoundException();
+
+    return response;
+  }
+}
