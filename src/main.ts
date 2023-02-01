f1cdd3935c1801b8cb25733223293f01bf8e6809
Andrei Karev <an.carev4@gmail.com>
2023-02-01 10:19:53 +0300

    feat: implement getting of the port value from the '.env'

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
+import { PORT } from './common/constants';
 
 async function bootstrap() {
+  dotenv.config({ path: '../.env' });
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT);
 }
+
 bootstrap();
