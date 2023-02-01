915340cf627b7d6dcb4c007d7dcdf2cd1b21bbf8
Andrei Karev <an.carev4@gmail.com>
2023-02-01 12:05:17 +0300

    feat: implement displaying of the swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import { cwd } from 'node:process';
+import { join } from 'node:path';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+
 import { AppModule } from './app.module';
 import { PORT } from './common/constants';
 
 async function bootstrap() {
-  dotenv.config({ path: '../.env' });
+  dotenv.config({ path: join(process.cwd(), '.env') });
   const app = await NestFactory.create(AppModule);
+  const document = await readFile(join(cwd(), 'doc', 'api.yaml'), 'utf-8');
+
+  SwaggerModule.setup('api', app, parse(document));
   await app.listen(PORT);
 }
 
