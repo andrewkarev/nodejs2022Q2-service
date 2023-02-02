c563a567e3eb27b587b98b464a700207f37f1150
Andrei Karev <an.carev4@gmail.com>
2023-02-02 15:55:53 +0300

    feat: implement `prepareAlbumResponse` function

---
+++ src/album/helpers/prepareAlbumResponse.ts
@@ -0,0 +1,23 @@
+import { Album } from '@prisma/client';
+
+class AlbumData {
+  public id: string;
+  public name: string;
+  public year: number;
+  public artistId: string | null;
+
+  constructor(data: Album) {
+    this.id = data.id;
+    this.name = data.name;
+    this.year = data.year;
+    this.artistId = data.artistId;
+  }
+}
+
+export const prepareAlbumResponse = (data: Album | Album[]) => {
+  if (Array.isArray(data)) {
+    return data.map((item) => new AlbumData(item));
+  } else {
+    return new AlbumData(data);
+  }
+};
