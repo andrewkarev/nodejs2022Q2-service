5c213406b256654b1b53d7d54e27c33183c93e83
Andrei Karev <an.carev4@gmail.com>
2023-02-26 16:06:44 +0300

    feat: add a variable with the path to the folder with logs

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,1 +1,7 @@
+import { cwd } from 'process';
+import { join } from 'path';
+
 export const PRISMA_ERROR = 'P2025';
+
+export const LOGS_FOLDER = 'logs';
+export const LOGS_FOLDER_PATH = join(cwd(), LOGS_FOLDER);
