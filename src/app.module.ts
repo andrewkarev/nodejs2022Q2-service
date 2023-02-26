673a160a0c4eccb150f3c7cd67ecb6df5becfc69
Andrei Karev <an.carev4@gmail.com>
2023-02-26 16:17:21 +0300

    feat: apply logging functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TrackModule } from './track/track.module';
 import { UserModule } from './user/user.module';
 import { ArtistModule } from './artist/artist.module';
@@ -9,6 +9,8 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './common/guards';
+import { LoggerMiddleware } from './common/utils/logging.middleware';
+import { LoggingModule } from './common/utils/logging.module';
 
 @Module({
   imports: [
@@ -20,8 +22,13 @@ import { AtGuard } from './common/guards';
     PrismaModule,
     ConfigModule.forRoot({ isGlobal: true }),
     AuthModule,
+    LoggingModule,
   ],
   controllers: [],
   providers: [{ provide: APP_GUARD, useClass: AtGuard }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,36 @@ import { readFile } from 'fs/promises';
 import { parse } from 'yaml';
 
 import { AppModule } from './app.module';
+import { LoggingService } from './common/utils/logging.service';
+import { HttpExceptionFilter } from './common/utils/http-exception.filter';
+import { createLogsFolder } from './common/helpers/createLogsFolder';
 
 async function bootstrap() {
-  dotenv.config({ path: join(process.cwd(), '.env') });
+  dotenv.config({ path: join(cwd(), '.env') });
   const app = await NestFactory.create(AppModule);
   const document = await readFile(join(cwd(), 'doc', 'api.yaml'), 'utf-8');
 
+  app.useLogger(app.get(LoggingService));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   SwaggerModule.setup('doc', app, parse(document));
+
+  const logError = (message: string) => {
+    const logger = new LoggingService();
+    logger.error(message);
+    process.exit(1);
+  };
+
+  process.on('uncaughtException', (error) => {
+    logError(`Uncaught ${error.name}: ${error.message}\n${error.stack}`);
+  });
+
+  process.on('unhandledRejection', (reason: Error) => {
+    logError(`Unhandled promise rejection: ${reason.message}\n${reason.stack}`);
+  });
+
+  await createLogsFolder();
+
   await app.listen(process.env.PORT || 4000);
 }
 
